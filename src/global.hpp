51690dcd676bcdd74ccbe2dcf4be003b0f481caf
saltyfedor <fedor.stryapunin@gmail.com>
2024-05-17 01:07:26 +0200

    made some constants global

---
+++ src/global.hpp
@@ -0,0 +1,12 @@
+#define SCREEN_HEIGHT 64
+#define SCREEN_WIDTH 128
+
+#define JUMP_SPEED 1
+#define SHIFT_SPEED_PX 2
+
+enum InputType
+{
+    UP = 10,
+};
+
+typedef bool Screen[SCREEN_WIDTH][SCREEN_HEIGHT];
