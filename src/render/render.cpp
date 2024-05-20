7ab47f13429ca492b4fc93408a55a0ee578f1a09
saltyfedor <fedor.stryapunin@gmail.com>
2024-05-20 22:43:46 +0200

    fixed debug

--- src/render/render.cpp
+++ src/render/render.cpp
@@ -22,8 +22,14 @@ void Render::displayDebug(){
 
     GFXcanvas1 canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
     canvas.fillScreen(BLACK);
-    canvas.setCursor(0, 24);
-    canvas.print("test");
+    canvas.print("Motion: ");
+    canvas.println(gameState->motion);
+    canvas.print("Pos Y: ");
+    canvas.println(gameState->playerPosition);
+    canvas.print("Events: ");
+    canvas.println(queue->getEventCount());
+    canvas.print("Preview input: ");
+    canvas.println(queue->previewEvent(0).input);
     display->drawBitmap(0, 0, canvas.getBuffer(), canvas.width(), canvas.height(), WHITE, BLACK);
     display->display();
 }
